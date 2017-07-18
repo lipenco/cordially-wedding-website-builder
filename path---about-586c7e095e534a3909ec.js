c668f26d252239ead728b76ce394ae306470933e
Magdalena Lipińska <mag.lipinska@gmail.com>
2017-07-18 16:22:42 -0700

    Updates

--- path---about-586c7e095e534a3909ec.js
+++ path---about-586c7e095e534a3909ec.js
@@ -1,2 +1,2 @@
-webpackJsonp([14],{"./node_modules/json-loader/index.js!./.cache/json/about.json":function(o,n){o.exports={pathContext:{}}}});
+webpackJsonp([15],{"./node_modules/json-loader/index.js!./.cache/json/about.json":function(o,n){o.exports={pathContext:{}}}});
 //# sourceMappingURL=path---about-586c7e095e534a3909ec.js.map

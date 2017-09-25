aea445412cdd594d11eedfbf26e141f24a15997a
Magdalena Lipińska <mag.lipinska@gmail.com>
2017-09-25 15:06:30 +0200

    Updates

---
+++ path---pricing-586c7e095e534a3909ec.js
@@ -0,0 +1,2 @@
+webpackJsonp([12],{"./node_modules/json-loader/index.js!./.cache/json/pricing.json":function(n,o){n.exports={pathContext:{}}}});
+//# sourceMappingURL=path---pricing-586c7e095e534a3909ec.js.map

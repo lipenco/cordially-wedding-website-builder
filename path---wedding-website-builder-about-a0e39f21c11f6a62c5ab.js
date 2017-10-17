c0c94be5c35e0265eebf52402ce5268148c7e9ba
Magdalena Lipińska <mag.lipinska@gmail.com>
2017-10-17 20:40:50 +0200

    Updates

--- path---wedding-website-builder-about-a0e39f21c11f6a62c5ab.js
+++ path---wedding-website-builder-about-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xbe85081a0826],{485:function(t,e){t.exports={pathContext:{}}}});
+webpackJsonp([0xbe85081a0826],{477:function(t,e){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---wedding-website-builder-about-a0e39f21c11f6a62c5ab.js.map

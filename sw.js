e0ce6c48333f1b2cf9b28f20206a6b488929cc0f
Magdalena Lipińska <mag.lipinska@gmail.com>
2017-10-18 12:30:58 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-18e93cdb3cd14801c7d2.js","247dd939cd3163660695a5e41f0ec824"],["/app-27d4a4a0698457ca2fbf.js","2f1badf72d0be08962cdeef7f8e37834"],["/app-2d2d95664802297caf65.js","7bc0f7e48a7f8346a9a896938ee6dc06"],["/app-53c54cf453a9e92ba7c1.js","e7a014b81f3e223dbe8e0d96ec541957"],["/commons-30342c0b4191e701e47e.js","9d14eacf88ae6785a9d128b5e24dcfeb"],["/index.html","9b641604d3d6a67284af22524f0cd5f2"],["/manifest.json","48bf1d52d81c92458992b8411a2b1c6b"],["/offline-plugin-app-shell-fallback/index.html","d38a91bebe522a13fe896adbbe6333c3"]];
+var precacheConfig = [["/app-18e93cdb3cd14801c7d2.js","247dd939cd3163660695a5e41f0ec824"],["/app-27d4a4a0698457ca2fbf.js","2f1badf72d0be08962cdeef7f8e37834"],["/app-2d2d95664802297caf65.js","7bc0f7e48a7f8346a9a896938ee6dc06"],["/app-53c54cf453a9e92ba7c1.js","e7a014b81f3e223dbe8e0d96ec541957"],["/commons-0b7c8175d2fd6947e04b.js","4bac64c209e54233292b5a20c925b50e"],["/commons-30342c0b4191e701e47e.js","9d14eacf88ae6785a9d128b5e24dcfeb"],["/index.html","6b94b0582c04acff2a46d78bad6679ef"],["/manifest.json","5f0b53ec95136a29a73bb40c030bbe3b"],["/offline-plugin-app-shell-fallback/index.html","6b32e3523fa9a783c3e43e7b2969de10"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 

1fb642ac0a88b79b20e8c80ed873bd59069e2be4
Magdalena Lipinska <mag.lipinska@gmail.com>
2018-02-08 11:57:57 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-588e8e34c1194e8d0858.js","47f31dec122f46d23a42008d986aa788"],["/commons-32d029ee0c3aa5adb7ea.js","e791f9bbb134a7102302f173eef8b042"],["/index.html","d10870ab4ef1bbdc9f9ae95283a2fd11"],["/manifest.json","6a3fce30e73ced6abea7c29616ffe506"],["/offline-plugin-app-shell-fallback/index.html","371a340925ba3c3b4af32b9177c7c51d"]];
+var precacheConfig = [["/app-0f2241cf683ece60185e.js","752e574e6b9d59041bfa73e33cd206a3"],["/commons-32d029ee0c3aa5adb7ea.js","e791f9bbb134a7102302f173eef8b042"],["/index.html","b224ac3e960a5bdfc51f0a0bcaa8abbb"],["/manifest.json","6a3fce30e73ced6abea7c29616ffe506"],["/offline-plugin-app-shell-fallback/index.html","11b13882b7768e405d7a0a0a9fb56439"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
